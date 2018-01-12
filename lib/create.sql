aa172d4f2e0a22bf1b60a51b309b1078b618319f
James Ferguson <ncjamesferguson@gmail.com>
2018-01-12 17:36:17 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,7 +1,21 @@
 CREATE TABLE projects(
   id INTEGER PRIMARY KEY,
+  title TEXT,
   category TEXT,
   funding_goal TEXT,
   start_date TEXT,
   end_date TEXT
 );
+
+CREATE TABLE users(
+  ID INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY,
+  amount REAL,
+  user_id INTEGER,
+  project_id INTEGER
+)
